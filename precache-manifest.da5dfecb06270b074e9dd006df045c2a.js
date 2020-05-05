78b7127296e82ad4709664613d75804057c818c2
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-05-05 20:27:47 +0300

    Updates

---
+++ precache-manifest.da5dfecb06270b074e9dd006df045c2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc9c4866c16d08d4bf4f21b89b7ed0a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f4ed27355d63a6dd1f4e",
+    "url": "./static/css/2.df39bf3c.chunk.css"
+  },
+  {
+    "revision": "915120457a0455e98e5e",
+    "url": "./static/css/main.822b64da.chunk.css"
+  },
+  {
+    "revision": "f4ed27355d63a6dd1f4e",
+    "url": "./static/js/2.a5f1c2ef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a5f1c2ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "915120457a0455e98e5e",
+    "url": "./static/js/main.1c41ed05.chunk.js"
+  },
+  {
+    "revision": "93138561b2093b9a2516",
+    "url": "./static/js/runtime-main.8aef587d.js"
+  }
+]);
